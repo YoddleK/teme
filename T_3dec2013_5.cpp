667c143e8f68d050d5ea0d055fabbc22f5b62984
YoddleK <bgd.aca55a@gmail.com>
2013-12-03 16:26:07 -0600

    use goto

--- T_3dec2013_5.cpp
+++ T_3dec2013_5.cpp
@@ -1,41 +1,37 @@
 #include <iostream>
 #include <math.h>
+
 using namespace std;
 //5. Se citesc numere atata timp cat sunt mai mari decat 100.
 //Numarati cate dintre valorile citite sunt numere prime.
+
 int main (int argc, char* argv[])
 {
     const int N = 5;
     int a[N] = {131, 111, 171, 200, 101};
     int count = 0, n;
 
-    for(n = 0; i < N; n++) {
+    for(n = 0; n < N; n++) {
         cout << "a[" << n << "]="; cin >> a[n];
         if(a[n] < 100)
             break;
     }
 
     for(int i=0; i < n; i++) {
-        cout << "\t>> A " << a[i] << endl;
 
-        if(a[i] % 2 == 0) {
-            cout << "modulo 2\n";
+         if(a[i] % 2 == 0) {
             continue;
         }
 
-        int flag = 0;
-        for(int div = 3; div < sqrt(a[i]) && !flag; div += 2) {
-            cout << ", " << div;
+        for(int div = 3; div < sqrt(a[i]); div += 2) {
         	if(a[i] % div == 0) {
-                cout << "\n modulo " << div << endl;
-                flag = 1;
+                goto flag;
         	}
         }
-        if(flag) continue;
 
         count ++;
+        flag: continue;
     }
 
     cout << ">> count = " << count << endl;
-
 }
